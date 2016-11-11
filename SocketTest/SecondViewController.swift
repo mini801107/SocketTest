ef5f5e37bb38b8914739b9d2d8666ed6fa5d671c
蔡佳旅 <mini801107@yahoo.com.tw>
2016-11-11 21:49:32 +0800

    Initial Commit

---
+++ SocketTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SocketTest
+//
+//  Created by 蔡佳旅 on 2016/11/11.
+//  Copyright © 2016年 蔡佳旅. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
